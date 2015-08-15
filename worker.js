50c20a9c8c8ab83f8662664c9fac3bc8165b39e5
NT <niranjan.thanikachalam@epfl.ch>
2015-08-15 11:37:32 +0200

    tryW

---
+++ worker.js
@@ -0,0 +1,11 @@
+onmessage = function(e) {
+var base="./frames_for_demo/frame";
+  for (var i = 0; i < 2500; ++i) {
+       var img = new Image();
+       img.src = base.concat(i.toString(),".png");
+  		 var ii=i/25;
+       var workerResult = ii.toString();
+       console.log('Posting message back to main script');
+       postMessage(workerResult);
+    }
+}
