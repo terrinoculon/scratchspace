0d7bfd8d807607d6e73cbd91c217e25d59f1a191
NT <niranjan.thanikachalam@epfl.ch>
2015-08-16 10:05:30 +0200

    tryktojlfdt

--- worker.js
+++ worker.js
@@ -1,11 +1,11 @@
 function e() {
 var base="http://terrinoculon.github.io/scratchspace/frames_for_demo/frame";
-  for (var i = 0; i < 2500; ++i) {
+  for (var i = 1; i < 2501; i++) {
     var url = base.concat(i.toString(),".png");
     var xhr = new XMLHttpRequest();
      xhr.responseType = 'blob';
     // xhr.onload = xhr.onerror = onload;
-     xhr.open('GET', url, true);
+     xhr.open('GET', url, false);
      xhr.send();
 
 
